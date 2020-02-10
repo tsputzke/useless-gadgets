af7a69d2a233a2565fbf6f9d3d5f3566a1339838
Timothy Putzke <tsputzke@gmail.com>
2020-02-09 20:16:37 -0500

    Implement canvas context inside React component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const ctx = this.refs.canvas.getContext('2d');
+    ctx.fillStyle = '#f00'
+    ctx.fillRect(20, 20, 100, 100);
+  }
+  
+  render() {
+    return (
+      <main className='App'>
+        <canvas ref='canvas'>
+  
+        </canvas>
+      </main>
+    )
+  }
 }
 
 export default App;
